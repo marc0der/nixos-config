396a25ab81adb7cee7f31844de94b5dac79ff9b1
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-02-02 14:25:26 +0000

    Initial revision

---
+++ common.nix
@@ -0,0 +1,84 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "marco";
+  home.homeDirectory = "/home/marco";
+  home.stateVersion = "24.11";
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    # packages
+    autojump
+    bat
+    borgbackup
+    fzf
+    gh
+    htop
+    httpie
+    nodePackages.jsonlint
+    lazygit
+    mpv
+    ncdu
+    nmap
+    nixfmt-rfc-style
+    pinentry
+    rclone
+    ripgrep
+    speedtest-rs
+    vscode
+    yamllint
+    zsh-powerlevel10k
+
+    # fonts
+    font-awesome
+    noto-fonts
+    noto-fonts-emoji
+    jetbrains-mono
+    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" "Noto" ]; })
+  ];
+
+  home.file = {
+    # ".gnupg/gpg-agent.conf".source = gnupg/gpg-agent.conf;
+    # ".gnupg/gpg.conf".source = gnupg/gpg.conf;
+
+    # hyprland
+    # ".config/hypr/hyprland.conf".source = hypr/hyprland.conf;
+
+    # kitty
+    # ".config/kitty/kitty.conf".source = kitty/kitty.conf;
+
+    # dunst
+    # ".config/dunst/dunstrc".source = dunst/dunstrc;
+
+    # waybar
+    # ".config/waybar/config.jsonc".source = waybar/config.jsonc;
+    # ".config/waybar/style.css".source = waybar/style.css;
+
+    # kanshi
+    # ".config/systemd/user/kanshi.service".source = systemd/kanshi.service;
+
+    # foot
+    # ".config/foot/foot.ini".source = foot/foot.ini;
+
+    # bin scripts
+    # "bin/next-wallpaper.sh".source = bin/next-wallpaper.sh;
+    # "bin/previous-wallpaper.sh".source = bin/previous-wallpaper.sh;
+
+  #   ".local/share/applications/discord.desktop".source = desktop/discord.desktop;
+  #   ".local/share/applications/todoist.desktop".source = desktop/todoist.desktop;
+  };
+
+  fonts.fontconfig.enable = true;
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD = "true";
+  };
+
+  home.sessionPath = ["$HOME/bin"];
+
+  home.changes-report.enable = true;
+
+  programs.home-manager.enable = true;
+}
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Modular NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        xenomorph = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
+      };
+      homeConfigurations = {
+        marco = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./common.nix
+            # ./git.nix
+            # ./report.nix
+            # ./xenomorph.nix
+            # ./zsh.nix
+          ];
+        };
+      };
+  };
+}
