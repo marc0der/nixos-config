5c9c053d36413a19a973706baa03104e3a8697f1
Marco Vermeulen <marco.vermeulen@protonmail.com>
2025-02-11 22:13:41 +0000

    Install 1password

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports =
@@ -96,6 +96,11 @@
     wofi
   ];
 
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "1password-gui"
+    "1password"
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
@@ -163,6 +168,15 @@
       background = "/etc/regreet/wall1.png";
       background-fit = "cover";
     };
+  };
+
+  # 1password
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "yourUsernameHere" ];
   };
 
   # Fix PAM authentication (important for logging in)
