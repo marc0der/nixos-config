53c4753ab6e824420e402de60227c42c49d8e59a
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-03-01 22:31:02 +0000

    Remove obsolete unfree predicate for 1password

--- configuration.nix
+++ configuration.nix
@@ -180,13 +180,6 @@ in
   services.gvfs.enable = true;
 
   # 1password
-  nixpkgs.config.allowUnfreePredicate =
-    pkg:
-    builtins.elem (lib.getName pkg) [
-      "1password-gui"
-      "1password"
-    ];
-
   programs._1password.enable = true;
   programs._1password-gui = {
     enable = true;
